da2af1ed32a61d07c247a1a77fd138d9fb9a5e79
przemo2608 <przemo95732@gmail.com>
2020-12-21 04:57:43 +0100

    get questions

--- src/views/logged/Reviews/ReviewForm.jsx
+++ src/views/logged/Reviews/ReviewForm.jsx
@@ -1,18 +1,43 @@
 import React from 'react';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 import styles from './index.module.scss';
 
 const propTypes = {
     props: PropTypes.object,
 };
+const token = JSON.parse(localStorage.getItem('token'));
 
-const ReviewForm = () => {
-    return (
+axios.interceptors.request.use(
+    config => {
+        config.headers.authorization = `Bearer ${token.value}`;
+        return config;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+);
+
+class ReviewForm extends React.Component {
+    state = {
+        questions: [],
+    }
+     componentDidMount() {
+        axios.get('https://grudlann-app.herokuapp.com/api/questions/answers',)
+        .then((response) => {
+            console.log(response);
+            this.setState({
+                questions: response.data,
+            });
+        }).catch(err => console.log(err.message));
+    }
+  render() { return (
         <div className={styles.wrapper}>
             <div className={styles.container}>
             </div>
         </div>
     );
+  };
 };
 
 ReviewForm.propTypes = propTypes;
